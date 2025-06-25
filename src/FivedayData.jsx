17ab8d8dcd15213df4c53bc83ed211ba34874c3c
Aryankarmur <154599865+Aryankarmur@users.noreply.github.com>
2025-06-25 21:52:24 +0530

    bug fixed

--- src/FivedayData.jsx
+++ src/FivedayData.jsx
@@ -1,185 +1,203 @@
 import "./assets/css/FivedayData.css";
+import Clearsky from "./assets/weather icons/clear-sky.png";
+import Mainlyclear from "./assets/weather icons/cloudy.png";
+import Partlycloudy from "./assets/weather icons/partly-cloudy.png";
+import Fog from "./assets/weather icons/fog.png";
+import Depositingrimefog from "./assets/weather icons/rimefog.png";
+import Snow from "./assets/weather icons/snow.png";
+import Litedrizzle from "./assets/weather icons/litedrizzle.png";
+import Drizzle from "./assets/weather icons/drizzle.png";
+import Rain from "./assets/weather icons/rain.png";
+import Snowshowers from "./assets/weather icons/snowshowers.png";
+import Freezingdrizzle from "./assets/weather icons/freezingdrizzle.png";
+import Freezingrain from "./assets/weather icons/freezing-rain.png";
+import Lightrain from "./assets/weather icons/lightrain.png";
+import Lightsnow from "./assets/weather icons/lightsnow.png";
+import Thundershowers from "./assets/weather icons/thunderstorm.png";
+import Thundershowershail from "./assets/weather icons/thunderstorm&hail.png";
+import Thundershowersheavyhail from "./assets/weather icons/thunderstorm&heavy hail.png";
+import Rainshowers from "./assets/weather icons/rainshowers.png";
 
 const FivedayData = ({ fivedayWeatherData }) => {
       
     //  Function to render the 5-day weather data
     function getWeatherData(code) {
-  switch (code) {
-    case 0:
-      return {
-        imgpath: "/src/assets/weather icons/clear-sky.png",
-        imgalt: "Clear sky",
-        weather: "Clear sky",
-      };
-    case 1:
-      return {
-        imgpath: "/src/assets/weather icons/cloudy.png",
-        imgalt: "Mainly clear",
-        weather: "Mainly clear",
-      };
-    case 2:
-      return {
-        imgpath: "/src/assets/weather icons/partly-cloudy.png",
-        imgalt: "Partly cloudy",
-        weather: "Partly cloudy",
-      };
-    case 3:
-      return {
-        imgpath: "/src/assets/weather icons/cloudy.png",
-        imgalt: "Overcast",
-        weather: "Overcast",
-      };
-    case 45:
-      return {
-        imgpath: "/src/assets/weather icons/fog.png",
-        imgalt: "fog",
-        weather: "fog",
-      };
-    case 48:
-      return {
-        imgpath: "/src/assets/weather icons/rimefog.png",
-        imgalt: "Depositing rime fog",
-        weather: "Depositing rime fog",
-      };
-    case 51:
-      return {
-        imgpath: "/src/assets/weather icons/litedrizzle.png",
-        imgalt: "Light drizzle",
-        weather: "Light drizzle",
-      };
-    case 53:
-      return {
-        imgpath: "/src/assets/weather icons/drizzle.png",
-        imgalt: "Moderate drizzle",
-        weather: "Moderate drizzle",
-      };
-    case 55:
-      return {
-        imgpath: "/src/assets/weather icons/drizzle.png",
-        imgalt: "Dense drizzle",
-        weather: "Dense drizzle",
-      };
-    case 56:
-      return {
-        imgpath: "/src/assets/weather icons/freezingdrizzle.png",
-        imgalt: "Light freezing drizzle",
-        weather: "Light freezing drizzle",
-      };
-    case 57:
-      return {
-        imgpath: "/src/assets/weather icons/freezingdrizzle.png",
-        imgalt: "Dense freezing drizzle",
-        weather: "Dense freezing drizzle",
-      };
-    case 61:
-      return {
-        imgpath: "/src/assets/weather icons/lightrain.png",
-        imgalt: "Light rain",
-        weather: "Light rain",
-      };
-    case 63:
-      return {
-        imgpath: "/src/assets/weather icons/rain.png",
-        imgalt: "Moderate rain",
-        weather: "Moderate rain",
-      };
-    case 65:
-      return {
-        imgpath: "/src/assets/weather icons/rain.png",
-        imgalt: "Heavy rain",
-        weather: "Heavy rain",
-      };
-    case 66:
-      return {
-        imgpath: "/src/assets/weather icons/freezing-rain.png",
-        imgalt: "Light freezing rain",
-        weather: "Light freezing rain",
-      };
-    case 67:
-      return {
-        imgpath: "/src/assets/weather icons/freezing-rain.png",
-        imgalt: "Heavy freezing rain",
-        weather: "Heavy freezing rain",
-      };
-    case 71:
-      return {
-        imgpath: "/src/assets/weather icons/lightsnow.png",
-        imgalt: "Light snow",
-        weather: "Light snow",
-      };
-    case 73:
-      return {
-        imgpath: "/src/assets/weather icons/snow.png",
-        imgalt: "Moderate snow",
-        weather: "Moderate snow",
-      };
-    case 75:
-      return {
-        imgpath: "/src/assets/weather icons/snow.png",
-        imgalt: "Heavy snow",
-        weather: "Heavy snow",
-      };
-    case 77:
-      return {
-        imgpath: "/src/assets/weather icons/snow.png",
-        imgalt: "Snow grains",
-        weather: "Snow grains",
-      };
-    case 80:
-      return {
-        imgpath: "/src/assets/weather icons/rainshowers.png",
-        imgalt: "Light rain showers",
-        weather: "Light rain showers",
-      };
-    case 81:
-      return {
-        imgpath: "/src/assets/weather icons/rainshowers.png",
-        imgalt: "Moderate rain showers",
-        weather: "Moderate rain showers",
-      };
-    case 82:
-      return {
-        imgpath: "/src/assets/weather icons/rainshowers.png",
-        imgalt: "Violent rain showers",
-        weather: "Violent rain showers",
-      };
-    case 85:
-      return {
-        imgpath: "/src/assets/weather icons/snowshowers.png",
-        imgalt: "Light snow showers",
-        weather: "Light snow showers",
-      };
-    case 86:
-      return {
-        imgpath: "/src/assets/weather icons/snowshowers.png",
-        imgalt: "Heavy snow showers",
-        weather: "Heavy snow showers",
-      };
-    case 95:
-      return {
-        imgpath: "/src/assets/weather icons/thunderstorm.png",
-        imgalt: "Thunderstorm, slight or moderate",
-        weather: "Thunderstorm, slight or moderate",
-      };
-    case 96:
-      return {
-        imgpath: "/src/assets/weather icons/thunderstorm&hail.png",
-        imgalt: "Thunderstorm with slight hail",
-        weather: "Thunderstorm with slight hail",
-      };
-    case 99:
-      return {
-        imgpath: "/src/assets/weather icons/thunderstorm&heavy hail.png",
-        imgalt: "Thunderstorm with heavy hail",
-        weather: "Thunderstorm with heavy hail",
-      };
-    default:
-      return {
-        imgpath: "/src/assets/weather icons/clear-sky.png",
-        imgalt: "Unknown weather",
-        weather: "Unknown weather",
-      };
-  }
+    switch (code) {
+      case 0:
+        return {
+          imgpath: Clearsky,
+          imgalt: "Clear sky",
+          weather: "Clear sky",
+        };
+      case 1:
+        return {
+          imgpath: Mainlyclear,
+          imgalt: "Mainly clear",
+          weather: "Mainly clear",
+        };
+      case 2:
+        return {
+          imgpath: Partlycloudy,
+          imgalt: "Partly cloudy",
+          weather: "Partly cloudy",
+        };
+      case 3:
+        return {
+          imgpath: Mainlyclear,
+          imgalt: "Overcast",
+          weather: "Overcast",
+        };
+      case 45:
+        return {
+          imgpath: Fog,
+          imgalt: "fog",
+          weather: "fog",
+        };
+      case 48:
+        return {
+          imgpath: Depositingrimefog,
+          imgalt: "Depositing rime fog",
+          weather: "Depositing rime fog",
+        };
+      case 51:
+        return {
+          imgpath: Litedrizzle,
+          imgalt: "Light drizzle",
+          weather: "Light drizzle",
+        };
+      case 53:
+        return {
+          imgpath: Drizzle,
+          imgalt: "Moderate drizzle",
+          weather: "Moderate drizzle",
+        };
+      case 55:
+        return {
+          imgpath: Drizzle,
+          imgalt: "Dense drizzle",
+          weather: "Dense drizzle",
+        };
+      case 56:
+        return {
+          imgpath: Freezingdrizzle,
+          imgalt: "Light freezing drizzle",
+          weather: "Light freezing drizzle",
+        };
+      case 57:
+        return {
+          imgpath: Freezingdrizzle,
+          imgalt: "Dense freezing drizzle",
+          weather: "Dense freezing drizzle",
+        };
+      case 61:
+        return {
+          imgpath: Lightrain,
+          imgalt: "Light rain",
+          weather: "Light rain",
+        };
+      case 63:
+        return {
+          imgpath: Rain,
+          imgalt: "Moderate rain",
+          weather: "Moderate rain",
+        };
+      case 65:
+        return {
+          imgpath: Rain,
+          imgalt: "Heavy rain",
+          weather: "Heavy rain",
+        };
+      case 66:
+        return {
+          imgpath: Freezingrain,
+          imgalt: "Light freezing rain",
+          weather: "Light freezing rain",
+        };
+      case 67:
+        return {
+          imgpath: Freezingrain,
+          imgalt: "Heavy freezing rain",
+          weather: "Heavy freezing rain",
+        };
+      case 71:
+        return {
+          imgpath: Lightsnow,
+          imgalt: "Light snow",
+          weather: "Light snow",
+        };
+      case 73:
+        return {
+          imgpath: Snow,
+          imgalt: "Moderate snow",
+          weather: "Moderate snow",
+        };
+      case 75:
+        return {
+          imgpath: Snow,
+          imgalt: "Heavy snow",
+          weather: "Heavy snow",
+        };
+      case 77:
+        return {
+          imgpath: Snow,
+          imgalt: "Snow grains",
+          weather: "Snow grains",
+        };
+      case 80:
+        return {
+          imgpath: Rainshowers,
+          imgalt: "Light rain showers",
+          weather: "Light rain showers",
+        };
+      case 81:
+        return {
+          imgpath: Rainshowers,
+          imgalt: "Moderate rain showers",
+          weather: "Moderate rain showers",
+        };
+      case 82:
+        return {
+          imgpath: Rainshowers,
+          imgalt: "Violent rain showers",
+          weather: "Violent rain showers",
+        };
+      case 85:
+        return {
+          imgpath: Snowshowers,
+          imgalt: "Light snow showers",
+          weather: "Light snow showers",
+        };
+      case 86:
+        return {
+          imgpath: Snowshowers,
+          imgalt: "Heavy snow showers",
+          weather: "Heavy snow showers",
+        };
+      case 95:
+        return {
+          imgpath: Thundershowers,
+          imgalt: "Thunderstorm, slight or moderate",
+          weather: "Thunderstorm, slight or moderate",
+        };
+      case 96:
+        return {
+          imgpath: Thundershowershail,
+          imgalt: "Thunderstorm with slight hail",
+          weather: "Thunderstorm with slight hail",
+        };
+      case 99:
+        return {
+          imgpath: Thundershowersheavyhail,
+          imgalt: "Thunderstorm with heavy hail",
+          weather: "Thunderstorm with heavy hail",
+        };
+      default:
+        return {
+          imgpath: Clearsky,
+          imgalt: "Unknown weather",
+          weather: "Unknown weather",
+        };
+    }
   };
   
   const renderFivedayData = () => {
--- src/OneDayForcast.jsx
+++ src/OneDayForcast.jsx
@@ -1,178 +1,197 @@
 import "./assets/css/OneDayForcast.css";
+import Clearsky from "./assets/weather icons/clear-sky.png";
+import Mainlyclear from "./assets/weather icons/cloudy.png";
+import Partlycloudy from "./assets/weather icons/partly-cloudy.png";
+import Fog from "./assets/weather icons/fog.png";
+import Depositingrimefog from "./assets/weather icons/rimefog.png";
+import Snow from "./assets/weather icons/snow.png";
+import Litedrizzle from "./assets/weather icons/litedrizzle.png";
+import Drizzle from "./assets/weather icons/drizzle.png";
+import Rain from "./assets/weather icons/rain.png";
+import Snowshowers from "./assets/weather icons/snowshowers.png";
+import Freezingdrizzle from "./assets/weather icons/freezingdrizzle.png";
+import Freezingrain from "./assets/weather icons/freezing-rain.png";
+import Lightrain from "./assets/weather icons/lightrain.png";
+import Lightsnow from "./assets/weather icons/lightsnow.png";
+import Thundershowers from "./assets/weather icons/thunderstorm.png";
+import Thundershowershail from "./assets/weather icons/thunderstorm&hail.png";
+import Thundershowersheavyhail from "./assets/weather icons/thunderstorm&heavy hail.png";
+import Rainshowers from "./assets/weather icons/rainshowers.png";
+
 const OneDayForcast = ({ weatherData }) => {
   function getWeatherData(code) {
     switch (code) {
       case 0:
         return {
-          imgpath: "/src/assets/weather icons/clear-sky.png",
+          imgpath: Clearsky,
           imgalt: "Clear sky",
           weather: "Clear sky",
         };
       case 1:
         return {
-          imgpath: "/src/assets/weather icons/cloudy.png",
+          imgpath: Mainlyclear,
           imgalt: "Mainly clear",
           weather: "Mainly clear",
         };
       case 2:
         return {
-          imgpath: "/src/assets/weather icons/partly-cloudy.png",
+          imgpath: Partlycloudy,
           imgalt: "Partly cloudy",
           weather: "Partly cloudy",
         };
       case 3:
         return {
-          imgpath: "/src/assets/weather icons/cloudy.png",
+          imgpath: Mainlyclear,
           imgalt: "Overcast",
           weather: "Overcast",
         };
       case 45:
         return {
-          imgpath: "/src/assets/weather icons/fog.png",
+          imgpath: Fog,
           imgalt: "fog",
           weather: "fog",
         };
       case 48:
         return {
-          imgpath: "/src/assets/weather icons/rimefog.png",
+          imgpath: Depositingrimefog,
           imgalt: "Depositing rime fog",
           weather: "Depositing rime fog",
         };
       case 51:
         return {
-          imgpath: "/src/assets/weather icons/litedrizzle.png",
+          imgpath: Litedrizzle,
           imgalt: "Light drizzle",
           weather: "Light drizzle",
         };
       case 53:
         return {
-          imgpath: "/src/assets/weather icons/drizzle.png",
+          imgpath: Drizzle,
           imgalt: "Moderate drizzle",
           weather: "Moderate drizzle",
         };
       case 55:
         return {
-          imgpath: "/src/assets/weather icons/drizzle.png",
+          imgpath: Drizzle,
           imgalt: "Dense drizzle",
           weather: "Dense drizzle",
         };
       case 56:
         return {
-          imgpath: "/src/assets/weather icons/freezingdrizzle.png",
+          imgpath: Freezingdrizzle,
           imgalt: "Light freezing drizzle",
           weather: "Light freezing drizzle",
         };
       case 57:
         return {
-          imgpath: "/src/assets/weather icons/freezingdrizzle.png",
+          imgpath: Freezingdrizzle,
           imgalt: "Dense freezing drizzle",
           weather: "Dense freezing drizzle",
         };
       case 61:
         return {
-          imgpath: "/src/assets/weather icons/lightrain.png",
+          imgpath: Lightrain,
           imgalt: "Light rain",
           weather: "Light rain",
         };
       case 63:
         return {
-          imgpath: "/src/assets/weather icons/rain.png",
+          imgpath: Rain,
           imgalt: "Moderate rain",
           weather: "Moderate rain",
         };
       case 65:
         return {
-          imgpath: "/src/assets/weather icons/rain.png",
+          imgpath: Rain,
           imgalt: "Heavy rain",
           weather: "Heavy rain",
         };
       case 66:
         return {
-          imgpath: "/src/assets/weather icons/freezing-rain.png",
+          imgpath: Freezingrain,
           imgalt: "Light freezing rain",
           weather: "Light freezing rain",
         };
       case 67:
         return {
-          imgpath: "/src/assets/weather icons/freezing-rain.png",
+          imgpath: Freezingrain,
           imgalt: "Heavy freezing rain",
           weather: "Heavy freezing rain",
         };
       case 71:
         return {
-          imgpath: "/src/assets/weather icons/lightsnow.png",
+          imgpath: Lightsnow,
           imgalt: "Light snow",
           weather: "Light snow",
         };
       case 73:
         return {
-          imgpath: "/src/assets/weather icons/snow.png",
+          imgpath: Snow,
           imgalt: "Moderate snow",
           weather: "Moderate snow",
         };
       case 75:
         return {
-          imgpath: "/src/assets/weather icons/snow.png",
+          imgpath: Snow,
           imgalt: "Heavy snow",
           weather: "Heavy snow",
         };
       case 77:
         return {
-          imgpath: "/src/assets/weather icons/snow.png",
+          imgpath: Snow,
           imgalt: "Snow grains",
           weather: "Snow grains",
         };
       case 80:
         return {
-          imgpath: "/src/assets/weather icons/rainshowers.png",
+          imgpath: Rainshowers,
           imgalt: "Light rain showers",
           weather: "Light rain showers",
         };
       case 81:
         return {
-          imgpath: "/src/assets/weather icons/rainshowers.png",
+          imgpath: Rainshowers,
           imgalt: "Moderate rain showers",
           weather: "Moderate rain showers",
         };
       case 82:
         return {
-          imgpath: "/src/assets/weather icons/rainshowers.png",
+          imgpath: Rainshowers,
           imgalt: "Violent rain showers",
           weather: "Violent rain showers",
         };
       case 85:
         return {
-          imgpath: "/src/assets/weather icons/snowshowers.png",
+          imgpath: Snowshowers,
           imgalt: "Light snow showers",
           weather: "Light snow showers",
         };
       case 86:
         return {
-          imgpath: "/src/assets/weather icons/snowshowers.png",
+          imgpath: Snowshowers,
           imgalt: "Heavy snow showers",
           weather: "Heavy snow showers",
         };
       case 95:
         return {
-          imgpath: "/src/assets/weather icons/thunderstorm.png",
+          imgpath: Thundershowers,
           imgalt: "Thunderstorm, slight or moderate",
           weather: "Thunderstorm, slight or moderate",
         };
       case 96:
         return {
-          imgpath: "/src/assets/weather icons/thunderstorm&hail.png",
+          imgpath: Thundershowershail,
           imgalt: "Thunderstorm with slight hail",
           weather: "Thunderstorm with slight hail",
         };
       case 99:
         return {
-          imgpath: "/src/assets/weather icons/thunderstorm&heavy hail.png",
+          imgpath: Thundershowersheavyhail,
           imgalt: "Thunderstorm with heavy hail",
           weather: "Thunderstorm with heavy hail",
         };
       default:
         return {
-          imgpath: "/src/assets/weather icons/clear-sky.png",
+          imgpath: Clearsky,
           imgalt: "Unknown weather",
           weather: "Unknown weather",
         };
@@ -209,7 +228,6 @@ const OneDayForcast = ({ weatherData }) => {
                 ? 12
                 : adjustedTime.getHours()}
               {adjustedTime.getHours() >= 12 ? " PM" : " AM"}
-             
             </h3>
             <img
               src={weather.imgpath}
